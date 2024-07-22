245a32c1409b7eac10023796cedd09a04829ea5e
Alejandro Lagarde <c-alejandro.lagarde@charter.com>
2024-07-22 17:26:48 -0600

    Session 4 ready

--- apps/vue-app/src/app/App.vue
+++ apps/vue-app/src/app/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <PostView />
-  <!-- <PostDetailView /> -->
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="./assets/world.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top" />
+      </a>
+      <div>
+        <button class="btn btn-outline-warning me-2">Categories</button>
+        <button class="btn btn-outline-warning">Login</button>
+      </div>
+    </div>
+  </nav>
+  <router-view />
 </template>
 
 <script>
-import PostView from './modules/posts/views/PostView.vue';
-/* import PostDetailView from './modules/posts/views/PostDetailView.vue'; */
-
-export default {
-  components: {
-    PostView
-    /* PostDetailView */
-  }
-};
+export default {};
 </script>
